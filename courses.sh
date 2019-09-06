5644831c531478a8b54fbb499a9930bc43f570e1
Emmanuel Paul <pauldukz@gmail.com>
2019-09-06 16:13:16 +0300

    add web scraper

---
+++ courses.sh
@@ -0,0 +1,143 @@
+#!/bin/bash 
+php codecourse download:course learn-dart
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course learn-slim-4
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course tailwind-with-nuxt-js
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course laravel-teams
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course the-liker-project-2.0
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course multistep-forms-with-laravel
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course custom-laravel-collections
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course super-fast-data-nesting
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course vue.js-firebase-authenticaton
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course inline-vue-validation-in-under-10-minutes
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course create-a-laravel-scout-elasticsearch-driver
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course grouping-laravel-notifications
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course testing-vue
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course recreate-our-auto-hiding-sliding-navigation
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course better-laravel-database-notifications
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course dynamic-vue-components
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course infinite-scrolling-search-with-vue-and-algolia
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course magically-invoking-laravel-controller-methods
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course using-env-files-with-nuxt
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+php codecourse download:course new-in-laravel-5-7
+echo
+echo -n '************************************************************'
+echo
+echo 'processing.... next course'
+sleep 5s
+echo
+echo -n press Enter or cmd to exit 
+read terminate
---
+++ main.py
@@ -0,0 +1,52 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+print('************************************************************')
+print('*******************  Welcome to EC254 scraper  *************')
+print('************************************************************')
+def scrape():
+
+    # loop all pages
+    shellFilename = "courses.sh"
+    courseFilename = "courses.txt"
+    c = open(courseFilename, "w+", newline="")
+    f = open(shellFilename, "w+", newline="")
+    f.write("#!/bin/bash \r\n")
+    startPage = input('\tEnter START page number: ')
+    endPage = input('\tEnter END page number: ')
+    # loop all pages
+    index = 1;
+    for page in range(int(startPage), int(endPage) + 1, 1):
+        response = requests.get('https://codecourse.com/library/all?free=false&page=' + str(page))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        courses = soup.find_all('a', href=True)
+        # course
+        ignore = ''
+        for course in courses:
+            link = course['href']
+            # validate link
+            if re.search(r"^/courses/", link):
+                tag = link[9:len(link)]
+                if ignore != tag:
+                    cmd = "php codecourse download:course " + tag + "\r\n"
+                    f.write(cmd)
+                    c.write(tag + "\r\n")
+                    f.write("echo\r\necho -n '************************************************************'\r\n")
+                    f.write("echo\r\necho 'processing.... next course'\r\nsleep 5s\r\necho\r\n")
+                ignore = tag
+
+    f.write("echo -n press Enter or cmd to exit \r\n")
+    f.write("read terminate")
+    print('************************************************************')
+    print("*\tDONE: " + shellFilename + " and " + courseFilename + "files generated")
+    print('************************************************************')
+
+
+while 1:
+    scrape()
+    runAgain = input('Enter (yes|YES) to continue or press Enter or cmd to exit: ')
+    if runAgain.lower() == "yes":
+        print('*********************   New Run      ***********************')
+    else:
+        break
